d30f00884014c7ad2e78d03ffe6a4dc34b33a3ca
mckinnor <ryanmckinnon97@gmail.com>
2016-01-23 17:52:58 -0700

    Update Main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@
 
 int main() {
     // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    int myId = 104787486;
+    int myNum = myId%13;
+    std::cout << "Hello, world! My number is " << myNum << '!' << std::endl;
 
     return 0;
 }
